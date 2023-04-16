c6cc9522acb034cfd89004325cabf90159b02ed8
Ethan Shahbazian <shahbazianep@gmail.com>
2023-04-15 17:25:08 -0700

    Updated mobile detection to account for hydration

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,15 @@ import DesktopApp from "../components/DesktopApp.js";
 import MobileApp from "../components/MobileApp.js";
 import "../styles/index.css";
 
-import { BrowserView, MobileView } from "react-device-detect";
+import { BrowserView, MobileView, isMobile } from "react-device-detect";
 
 const IndexPage = () => {
-    return (
-        <div style={{ height: "100%" }}>
-            <BrowserView>
-                <DesktopApp />
-            </BrowserView>
-            <MobileView style={{ height: "100%" }}>
-                <MobileApp />
-            </MobileView>
-        </div>
-    );
+    const [mob, setMob] = React.useState(false);
+    React.useEffect(() => {
+        setMob(isMobile);
+    }, []);
+
+    return mob ? <MobileApp /> : <DesktopApp />;
 };
 
 export default IndexPage;
