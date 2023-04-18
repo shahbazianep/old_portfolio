11dc652fdc043c3f050ad7607a7111bff6e21a52
Ethan Shahbazian <shahbazianep@gmail.com>
2023-04-18 14:30:38 -0700

    Bugfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => {
         }
     }, []);
 
-    return mob ? <MobileApp /> : <DesktopApp />;
+    return mob ? <DesktopApp /> : <DesktopApp />;
 };
 
 export default IndexPage;
